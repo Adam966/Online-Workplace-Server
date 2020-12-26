5eb3c009feeb6e529b6f052e986027277d5cce64
Adam966 <adam.ivan@akademiasovy.sk>
2020-12-26 21:35:01 +0100

    initial commit

---
+++ src/main/java/com/kosickaakademia/onlineworkplaceserver/OnlineWorkplaceServerApplication.java
@@ -0,0 +1,13 @@
+package com.kosickaakademia.onlineworkplaceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWorkplaceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineWorkplaceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kosickaakademia/onlineworkplaceserver/OnlineWorkplaceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosickaakademia.onlineworkplaceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWorkplaceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
